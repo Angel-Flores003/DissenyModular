96d071d45b9d012e51a6c2cb8695d4825a856fca
Angel-Flores003 <angel-abraham.flores.7e8@itb.cat>
2025-12-02 11:57:47 +0100

    Complete Exercice Two

--- DissenyModular/Program.cs
+++ DissenyModular/Program.cs
@@ -7,18 +7,22 @@ public class Program
         Console.OutputEncoding = System.Text.Encoding.UTF8;
 
         const string MsgExercise = "Choose an option\n" +
-            "1 - Value in range\n" +
+            "1 - Change Temperature\n" +
             "0 - Exit";
         const string Error = "Invalid Input, please enter a 1 or a 0";
         const string Exit = "Exiting...";
         const string Wolf = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠁⠸⢳⡄⠀⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠃⠀⠀⢸⠸⠀⡠⣄⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠃⠀⠀⢠⣞⣀⡿⠀⠀⣧⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣠⡖⠁⠀⠀⠀⢸⠈⢈⡇⠀⢀⡏⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠩⢠⡴⠀⠀⠀⠀⠀⠈⡶⠉⠀⠀⡸⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⢀⠎⢠⣇⠏⠀⠀⠀⠀⠀⠀⠀⠁⠀⢀⠄⡇⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⢠⠏⠀⢸⣿⣴⠀⠀⠀⠀⠀⠀⣆⣀⢾⢟⠴⡇⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⢀⣿⠀⠠⣄⠸⢹⣦⠀⠀⡄⠀⠀⢋⡟⠀⠀⠁⣇⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⢀⡾⠁⢠⠀⣿⠃⠘⢹⣦⢠⣼⠀⠀⠉⠀⠀⠀⠀⢸⡀⠀⠀⠀⠀\r\n⠀⠀⢀⣴⠫⠤⣶⣿⢀⡏⠀⠀⠘⢸⡟⠋⠀⠀⠀⠀⠀⠀⠀⠀⢳⠀⠀⠀⠀\r\n⠐⠿⢿⣿⣤⣴⣿⣣⢾⡄⠀⠀⠀⠀⠳⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢣⠀⠀⠀\r\n⠀⠀⠀⣨⣟⡍⠉⠚⠹⣇⡄⠀⠀⠀⠀⠀⠀⠀⠀⠈⢦⠀⠀⢀⡀⣾⡇⠀⠀\r\n⠀⠀⢠⠟⣹⣧⠃⠀⠀⢿⢻⡀⢄⠀⠀⠀⠀⠐⣦⡀⣸⣆⠀⣾⣧⣯⢻⠀⠀\r\n⠀⠀⠘⣰⣿⣿⡄⡆⠀⠀⠀⠳⣼⢦⡘⣄⠀⠀⡟⡷⠃⠘⢶⣿⡎⠻⣆⠀⠀\r\n⠀⠀⠀⡟⡿⢿⡿⠀⠀⠀⠀⠀⠙⠀⠻⢯⢷⣼⠁⠁⠀⠀⠀⠙⢿⡄⡈⢆⠀\r\n⠀⠀⠀⠀⡇⣿⡅⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠦⠀⠀⠀⠀⠀⠀⡇⢹⢿⡀\r\n⠀⠀⠀⠀⠁⠛⠓⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠼⠇⠁";
-        const string Key = "Press any key to continue";        
-        const string MsgInput = "Enter a number to validate: ";
-        const string MaxRang = "Enter the maximum limit: ";
-        const string MinRang = "Enter the minimum limit: ";
-        const string InRange = "The number {0} is in range [{1},{2}]";
-        const string OutRange = "The number {0} isn't in range [{1},{2}]";
-        int value = 0, numMax = 0, numMin = 0,op = -1;
+        const string Key = "Press any key to continue";
+        const string EnterTemp = "Enter a decimal temperature: ";
+        const string TypeConvert = "1: Celsius a Fahrenheit\n" +
+            "2: Fahrenheit a Celsius\n" +
+            "3: Celsius a Kelvin";
+        const string NoAttempts = "Run ount of attempts";
+        const string ToFahr = "{0}ºC in Fahrenheit are {1}ºF";
+        const string ToCel = "{0}ºF in Celsius are {1}ºC";
+        const string ToKelv = "{0}ºC in Kelvin are {1}K";
+        int count, tempOp = 0, op = 0-1;
+        double temp = 0.0, tempChange;
 
         do
         {
@@ -35,14 +39,27 @@ public class Program
                         Console.ForegroundColor = ConsoleColor.White;
                         break;
                     case 1:
-                        numMin = validateInput(MinRang);
-                        numMax = validateInput(MaxRang);
-                        value = validateInput(MsgInput);
-                        if (numMax < numMin)
+                        count = 0;
+                        temp = validateInput(EnterTemp);
+                        validateOptioTemp(TypeConvert, count, ref tempOp);
+                        switch (tempOp)
                         {
-                            Swap(ref numMax, ref numMin);
+                            case 1:
+                                tempChange = temp * (9.0 / 5.0) + 32;
+                                WriteTemp(ToFahr, temp, tempChange);
+                                break;
+                            case 2:
+                                tempChange = (temp - 32.0) * (5.0 / 9.0);
+                                WriteTemp(ToCel, temp, tempChange);
+                                break;
+                            case 3:
+                                tempChange = temp + 273.15;
+                                WriteTemp(ToKelv, temp, tempChange);
+                                break;
+                            default:
+                                Console.WriteLine(NoAttempts);
+                                break;
                         }
-                        Console.WriteLine(isInRange(value, numMin, numMax) ? string.Format(InRange, value, numMin, numMax) : string.Format(OutRange, value, numMin, numMax));
                         Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine(Key);
                         Console.ForegroundColor = ConsoleColor.White;
@@ -75,26 +92,37 @@ public class Program
         Console.ForegroundColor = ConsoleColor.White;
         return value;
     }
-    public static int validateInput(string text)
+    public static double validateInput(string text)
     {
-        int value;
+        double value;
         do
         {
             Console.Write(text);
             Console.ForegroundColor= ConsoleColor.Green;
-        } while (!Int32.TryParse(Console.ReadLine(), out value));
+        } while (!Double.TryParse(Console.ReadLine(), out value));
         Console.ForegroundColor = ConsoleColor.White;
         return value;
     }
-    public static void Swap(ref int max, ref int min)
+    public static void validateOptioTemp(string TypeConvert, int count, ref int tempOp)
     {
-        int swap;
-        if (max < min)
+        do
         {
-            swap = max;
-            max = min;
-            min = swap;
-        }
+            tempOp = validateInput(TypeConvert, tempOp, ref count);
+        } while (!isInRange(tempOp, 1, 3) && count < 3);
+    }
+    public static int validateInput(string text, int value, ref int count)
+    {
+        do
+        {
+            Console.WriteLine(text);
+            count++;
+        } while (!Int32.TryParse(Console.ReadLine(), out value) && count < 3);
+        return value;
     }
     public static bool isInRange(int value, int min, int max) => value >= min && value <= max;
+    public static void WriteTemp(string text, double temp, double tempChange)
+    {
+        tempChange = Math.Round(tempChange, 2);
+        Console.WriteLine(text, temp, tempChange);
+    }
 }
